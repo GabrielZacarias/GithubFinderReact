754459aecdb2b9ecb26eefc4f182a517f5697543
Gabriel Zacarias <gabezacarias@outlook.com>
2021-08-08 09:38:26 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Fragment, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users.js';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import User from './components/users/User';
+
+import GithubState from './context/github/GithubState';
+import './App.css';
+
+function App() {
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (msg, type) => {
+    setAlert({msg, type});
+    setTimeout(() => setAlert(null), 5000);
+  }
+
+    return (
+      <GithubState>
+      <Router>
+        <div className='App'>
+
+          <Navbar />
+
+          <div className='container'>
+            <Alert alert={alert} />
+
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search setAlert={showAlert}/>
+                  <Users />
+                </Fragment>
+              )} />
+
+              <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' component={User} />
+            </Switch>
+          </div>
+          
+        </div>
+      </Router>
+      </GithubState>
+    );
+  }
+
+export default App;
